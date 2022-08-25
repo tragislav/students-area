d9839421f8781463c3dbfa026874537e15f95fd2
Stanislau Dykomenko <stanislau.dykomenko@effective-soft.com>
2022-08-25 10:23:07 +0300

    Added dependence for Login Page, added default css styles for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Layout from './components/Layout';
+import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route
-            index
-            element={<div className="App">Config husky again</div>}
-          />
+          <Route index element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
