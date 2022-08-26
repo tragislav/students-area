7afc7ea380542d9a68de80b307a84bc5de0f9b77
Stanislau Dykomenko <stanislau.dykomenko@effective-soft.com>
2022-08-26 10:39:53 +0300

    Added header component and styled it

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,50 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+
+import userPhoto from '../../assets/images/user-photo.svg';
+import {
+  MainHeader,
+  HeaderInner,
+  HeaderInnerTitle,
+  HeaderInnerTitleText,
+  HeaderInnerContent,
+  HeaderInnerContentUser,
+  ContentUserImage,
+  LogoutBtn,
+  ContentUserFIO,
+} from './styled';
+
 function Header() {
-  return <div>Header</div>;
+  const navigate = useNavigate();
+  const { user, signOut } = useAuth();
+
+  const logout = () => {
+    localStorage.removeItem('user');
+    signOut(() => navigate('/', { replace: true }));
+  };
+
+  return (
+    <MainHeader>
+      <HeaderInner>
+        <HeaderInnerTitle>
+          <Link to="/main">
+            <HeaderInnerTitleText>
+              Личный кабинет студента УО &#171;ВГТУ&#187;
+            </HeaderInnerTitleText>
+          </Link>
+        </HeaderInnerTitle>
+        {user && (
+          <HeaderInnerContent>
+            <HeaderInnerContentUser>
+              <ContentUserImage src={userPhoto} alt={user.fio} />
+              <ContentUserFIO>{user.fio}</ContentUserFIO>
+            </HeaderInnerContentUser>
+            <LogoutBtn type="submit" value="Выйти" onClick={logout} />
+          </HeaderInnerContent>
+        )}
+      </HeaderInner>
+    </MainHeader>
+  );
 }
 
 export default Header;
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,2 +1,65 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import styled from 'styled-components';
+
+export const MainHeader = styled.header``;
+
+export const HeaderInner = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  background: #434c63;
+`;
+
+export const HeaderInnerTitle = styled.div`
+  display: flex;
+  padding: 31px 0 31px 60px;
+`;
+
+export const HeaderInnerTitleText = styled.h1`
+  font-family: Roboto, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 23px;
+  line-height: 120%;
+  padding-left: 29px;
+  text-align: center;
+  color: #ffffff;
+`;
+
+export const HeaderInnerContent = styled.div`
+  display: flex;
+  padding: 0 60px 0 0;
+  width: 260px;
+  justify-content: space-between;
+`;
+
+export const HeaderInnerContentUser = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ContentUserImage = styled.img``;
+
+export const ContentUserFIO = styled.p`
+  font-family: Roboto, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 16px;
+  padding-left: 12px;
+  color: #ffffff;
+`;
+
+export const LogoutBtn = styled.input`
+  background: #434c63;
+  border: 1.5px solid #ffffff;
+  border-radius: 7px;
+  height: 42px;
+  width: 100px;
+  font-family: Roboto, sans-serif;
+  font-weight: normal;
+  font-size: 15px;
+  line-height: 18px;
+  color: #ffffff;
+  margin-top: 24px;
+`;
